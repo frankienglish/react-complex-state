505455422f6197e11ccb3636f7df66fdad8347a5
frankienglish <frankienglish0416@gmail.com>
2021-04-14 12:22:16 -0700

    used array de structuring to update the states of each component with one object

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [fullName, updateName] = useState({
+    fname: "",
+    lName: ""
+  });
+
+  function handleChange(event) {
+    const { value, name } = event.target;
+
+    updateName((previousValue) => {
+      if (name === "fName") {
+        return {
+          fName: value,
+          lName: previousValue.lName
+        };
+      } else if (name === "lName") {
+        return {
+          fName: previousValue.fName,
+          lName: value
+        };
+      }
+    });
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>
+        Hello {fullName.fName} {fullName.lName}{" "}
+      </h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+        <input
+          onChange={handleChange}
+          name="fName"
+          placeholder="First Name"
+          // value={fullName.fname}
+        />
+        <input
+          onChange={handleChange}
+          name="lName"
+          placeholder="Last Name"
+          //value={fullName.lName}
+        />
         <button>Submit</button>
       </form>
     </div>
